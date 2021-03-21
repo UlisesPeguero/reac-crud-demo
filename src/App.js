9a64e0a6c62c718c6a838fdce4158ad7ccb67f30
Fox <peguero.ulises@gmail.com>
2021-03-21 00:18:14 -0500

    Created header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import ErrorBoundary from './components/errorBoundary.component';
+import Header from './components/header.component';
+// css
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="Simple React CRUD"/>
+      <ErrorBoundary>
+        <h1>Content</h1>
+      </ErrorBoundary>
+    </>
   );
 }
 
